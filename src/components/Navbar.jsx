48455904c29557e0a0e32942ad90838d75e89aef
SantiTorija <torija.santiago@gmail.com>
2022-09-07 13:12:59 -0300

    navbar con offcanvas incluido

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,20 +4,39 @@ import Navbar from "react-bootstrap/Navbar";
 import "../styles/navbar.css";
 import { Link } from "react-router-dom";
 
+import OffCanvasCart from "./OffCanvasCart";
+
 function NavBar() {
   return (
-    <Navbar className="navbar" expand="lg">
-      <Container className="navbar-container">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link to={"/"}>Home</Link>
-            <Link to={""}>Tienda</Link>
-            <Link to={""}>Sobre nosotros</Link>
-            <Link to={""}>Contact</Link>
-          </Nav>
-        </Navbar.Collapse>
-        <Navbar.Brand href="#home">Hackwines</Navbar.Brand>
+    <Navbar className="navbar sticky-top" expand="lg">
+      <Container
+        fluid
+        className="navbar-container d-flex justify-content-between py-2"
+      >
+        <div className="d-flex">
+          <Navbar.Brand className="text-light nav-brand-icon">
+            Hackwines
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Link className="navbar-link me-3 ms-3" to={"/"}>
+                Home
+              </Link>
+              <Link className="navbar-link me-3" to={"/tienda"}>
+                Tienda
+              </Link>
+              <Link className="navbar-link me-3" to={"/sobre-este-proyecto"}>
+                Sobre este proyecto
+              </Link>
+              <Link className="navbar-link me-3" to={"/contacto"}>
+                Contacto
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
+
+        <OffCanvasCart placement={"end"} name={"end"} />
       </Container>
     </Navbar>
   );
