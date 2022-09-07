e3b2bfb94fee7e150596e0bb7b4268d5bbc2544b
SantiTorija <torija.santiago@gmail.com>
2022-09-07 14:55:26 -0300

    sidebar componente agregado

---
+++ src/pages/Shop.jsx
@@ -0,0 +1,10 @@
+import Sidebar from "../components/Sidebar";
+import NavBar from "../components/Navbar";
+function Shop() {
+  return (
+    <>
+      <NavBar />
+      <Sidebar />
+    </>
+  );
+}
