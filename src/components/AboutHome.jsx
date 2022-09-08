6abb8abe0c2fe1e0178210dcf709540e3d70bc4f
Guillermo <guillermogalain@gmail.com>
2022-09-08 09:10:05 -0300

    homeminor

--- src/components/AboutHome.jsx
+++ src/components/AboutHome.jsx
@@ -1,31 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/home.css";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
+import { InfoSquare } from "react-bootstrap-icons";
 
 function AboutHome() {
 	return (
-		<div className="aboutHome">
+		<div className="aboutHome d-flex flex-column align-items-center justify-content-center">
 			<Container>
-				<Row>
-					<Col className="d-flex flex-column align-items-center justify-content-center">
-						<div className="columAbout d-flex flex-column">
-							<h4 className="aboutText">
-								Proyecto realizado con fines academicos como trabajo final para
-								el coding Bootcamp de Hack Academy.
-							</h4>
-							<Button>Saber mas</Button>
-						</div>
-					</Col>
-					<Col>
-						<iframe
-							src="https://embed.lottiefiles.com/animation/55290"
-							title="prueba"
-						></iframe>
-					</Col>
-				</Row>
+				<div className="d-flex flex-column align-items-center justify-content-center">
+					<h4 className="aboutText">
+						Proyecto realizado con fines academicos como trabajo final para el
+						coding Bootcamp de Hack Academy.
+					</h4>
+					<a href="prueba">
+						<InfoSquare color="white">Saber mas</InfoSquare>
+					</a>
+				</div>
 			</Container>
 		</div>
 	);
