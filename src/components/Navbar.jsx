bd8906a4ea3fa939c4997155fa742550cf0f97f7
Guillermo <guillermogalain@gmail.com>
2022-09-08 17:32:27 -0300

    css changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,43 +3,39 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "../styles/navbar.css";
 import { Link } from "react-router-dom";
-
+import hackWines from "../assets/images/hackWines.png";
 import OffCanvasCart from "./OffCanvasCart";
 
 function NavBar() {
-  return (
-    <Navbar className="navbar sticky-top" expand="lg">
-      <Container
-        fluid
-        className="navbar-container d-flex justify-content-between py-2"
-      >
-        <div className="d-flex">
-          <Navbar.Brand className="text-light nav-brand-icon">
-            Hackwines
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Link className="navbar-link me-3 ms-3" to={"/"}>
-                Home
-              </Link>
-              <Link className="navbar-link me-3" to={"/tienda"}>
-                Tienda
-              </Link>
-              <Link className="navbar-link me-3" to={"/sobre-este-proyecto"}>
-                Sobre este proyecto
-              </Link>
-              <Link className="navbar-link me-3" to={"/contacto"}>
-                Contacto
-              </Link>
-            </Nav>
-          </Navbar.Collapse>
-        </div>
-
-        <OffCanvasCart placement={"end"} name={"end"} />
-      </Container>
-    </Navbar>
-  );
+	return (
+		<Navbar className="navbar sticky-top" expand="lg">
+			<Container className="navbar-container d-flex justify-content-between py-2">
+				<div className="d-flex">
+					<Navbar.Brand className="text-light nav-brand-icon">
+						<img alt="" src={hackWines} />
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="me-auto">
+							<Link className="navbar-link me-3 ms-3" to={"/"}>
+								Home
+							</Link>
+							<Link className="navbar-link me-3" to={"/tienda"}>
+								Tienda
+							</Link>
+							<Link className="navbar-link me-3" to={"/sobre-este-proyecto"}>
+								Sobre este proyecto
+							</Link>
+							<Link className="navbar-link me-3" to={"/contacto"}>
+								Contacto
+							</Link>
+						</Nav>
+					</Navbar.Collapse>
+				</div>
+				<OffCanvasCart placement={"end"} name={"end"} />
+			</Container>
+		</Navbar>
+	);
 }
 
 export default NavBar;
