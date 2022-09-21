7c1eadf6321501d4f6f1a0b3218d25132cd3bcc2
Guillermo <guillermogalain@gmail.com>
2022-09-21 16:18:16 -0300

    newimagesaboutandmore

--- src/components/AboutHero.jsx
+++ src/components/AboutHero.jsx
@@ -1,12 +1,14 @@
 import AboutTitle from "../components/AboutTitle";
 import Container from "react-bootstrap/Container";
+import ScrollIcon from "../components/ScrollIcon";
+
 function AboutHero() {
   return (
-    <div className="about__us__hero ">
+    <div className="about__us__hero">
       <Container>
         <div className="text-hero">
           <AboutTitle title={"Sobre este proyecto"} />
-          <p className="paragraph__hero mt-4 mb-5 ">
+          <p className="paragraph__hero mt-4 mb-4 ">
             Este proyecto surge como trabajo final del curso intensivo (coding Bootcamp) realizado
             de la mano de Hack Academy. <br />
             <br />
@@ -17,6 +19,9 @@ function AboutHero() {
             instancias de trabajo en equipo, nos preparamos de acuerdo a las necesidades del mercado
             para poder insertarnos y formar parte de la industria.
           </p>
+          <div className="w-100 d-flex justify-content-center">
+            <ScrollIcon />
+          </div>
         </div>
       </Container>
     </div>
---
+++ src/components/ScrollIcon.jsx
@@ -0,0 +1,13 @@
+import "../styles/scrollIcon.css";
+
+function ScrollIcon() {
+  return (
+    <div className="scroll-downs">
+      <div className="mousey">
+        <div className="scroller "></div>
+      </div>
+    </div>
+  );
+}
+
+export default ScrollIcon;
